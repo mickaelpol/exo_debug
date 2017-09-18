993e8d6cc44e838fa34ce666804becaffc0862bd
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-09-18 13:55:28 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var unElement = document.getElementById("box");
+console.log(mon element);
+console.log(unElement)
+
+unElement.remove();
---
+++ js/script.js
@@ -0,0 +1,17 @@
+fonction hello(){
+  alert('Hello');
+}
+
+function alerte('prenom'){
+  alert('Hello'+prenom);
+}
+
+function laBoucle(){
+  for(var i=0, i<=10, i++);
+  {
+    console.log(i);
+  }
+}
+
+laBoucle();
+alert('Pas mal d'erreurs corrigées, BRAVO');
